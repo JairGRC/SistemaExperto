31356a4bf26fc3477bcc3a27049fd4124de2f815
rrubioc15 <rmrc.libra15@gmail.com>
2021-09-17 11:55:45 -0500

    optimizando

--- mostrar_Resultado.php
+++ mostrar_Resultado.php
@@ -7,7 +7,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="css/estilosJhan.css">
     <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
@@ -27,7 +28,7 @@
             <a href="testvocacional.php" class="btn btn-primary">Volver a Intentar</a>
         </div>
     </div>
-
+    
 </body>
 
 </html>
